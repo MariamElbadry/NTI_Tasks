a20ec7f1a41280455f938000bb40b774644195a2
Mariam Elbadry <M.Sherif2434@nu.edu.eg>
2026-07-02 06:29:59 +0300

    Add files via upload

---
+++ S3_Tasks/index.js
@@ -0,0 +1,6 @@
+const add_task = require("./modules/add_task");
+const update_task = require("./modules/update_task");
+const remove_tasks = require("./modules/remove_tasks");
+const list_tasks = require("./modules/list_tasks");
+
+list_tasks();
