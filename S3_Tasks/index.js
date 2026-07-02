b5b755c786954049b0fefb5e8db3a48bd75de716
Mariam Elbadry <M.Sherif2434@nu.edu.eg>
2026-07-02 06:30:19 +0300

    Delete S3_Tasks/index.js

--- S3_Tasks/index.js
+++
@@ -1,6 +0,0 @@
-const add_task = require("./modules/add_task");
-const update_task = require("./modules/update_task");
-const remove_tasks = require("./modules/remove_tasks");
-const list_tasks = require("./modules/list_tasks");
-
-list_tasks();
