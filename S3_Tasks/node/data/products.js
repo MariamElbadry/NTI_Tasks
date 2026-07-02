5527ab035c78cab2327236b43df3f2c656863e4e
Mariam Elbadry <M.Sherif2434@nu.edu.eg>
2026-07-02 06:32:10 +0300

    Add files via upload

---
+++ S3_Tasks/node/data/products.js
@@ -0,0 +1,11 @@
+let products = [
+  { id: 1, name: "Sunscreen", price: 250},
+  { id: 2, name: "Cleanser", price: 145},
+  { id: 3, name: "Shampoo", price: 170},
+  { id: 4, name: "Conditioner", price: 180},
+  { id: 5, name: "Moisturizer", price: 150},
+  { id: 6, name: "Lotion", price: 210},
+  { id: 7, name: "Lip Pulm", price: 35},
+];
+
+module.exports = products;
